a50064624d8fb5251746df62e7f72dbeb6a8f26d
mattopolitan <mattopolitan@gmail.com>
2023-02-23 17:35:13 +0800

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,33 @@
-import { HardhatUserConfig } from "hardhat/config";
-
-const path = require('path')
-require('@nomiclabs/hardhat-waffle')
+import { HardhatUserConfig } from "hardhat/config"
+import path from 'path'
+import '@nomiclabs/hardhat-waffle'
 require('dotenv').config({ path: path.join(__dirname, '/.env.local') })
 require('hardhat-gas-reporter')
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!, process.env.ACCOUNT2_PRIVATE_KEY!]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!, process.env.ACCOUNT2_PRIVATE_KEY!]
+    }
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
 
 export default config;
